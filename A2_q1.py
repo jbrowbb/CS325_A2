b091fc9b24c3361542dfcef9528f11019bf8e4f4
Jada Brown <jbrowbb@siue.edu>
2025-06-11 15:06:55 -0500

    adding Question 1 (Basic Decorator for Logging)

---
+++ A2_q1.py
@@ -0,0 +1,19 @@
+import logging
+
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger(__name__)
+        logger.info(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
+        result = func(*args, **kwargs)
+        logger.info(f"Function {func.__name__} returned: {result}")
+        return result
+    return wrapper
+
+@log_function_call
+def add(x,y):
+    return x + y
+
+# set up logging
+logging.basicConfig(level=logging.INFO)
+
+result = add(x=2,y=3)   # for kwargs
